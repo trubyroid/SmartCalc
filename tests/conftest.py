44d562c6eee98dfee3b88075cc24e43a3da7c52c
trubyroid <andrkrestyaninov@gmail.com>
2024-12-02 03:28:55 +0300

    Made a new allure-report

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
+import allure
 from ..smartcalc.model.model import Model
 
 
 @pytest.fixture(scope="session")
+@allure.title("Создание инстанса класса Model")
 def calc_model():
     return Model()
